3d35d356ae63da6736aebd70a2579687e3c95cd1
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-07-22 13:13:07 +0300

    add async/await

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const refs = {
   formEl: document.querySelector('.search-form'),
   galleryEl: document.querySelector('.gallery'),
   brtMoreEl: document.querySelector('.load-more'),
+  endOfListEL: document.querySelector('.end-message'),
 };
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
@@ -12,40 +13,59 @@ const API_KEY = '38382218-99536ad77025498ca3a521a1b';
 const IMAGE_TYPE = 'photo';
 const IMAGE_ORIENTATION = 'horizontal';
 const IMAGE_SAFESEARCH = 'true';
+const NUMBER_OF_IMAGE = 40;
 let countOfPage = 1;
 let qValue = null;
+let totalHits = null;
 
-refs.brtMoreEl.setAttribute('hidden', 'true');
+refs.brtMoreEl.classList.toggle('hidden');
 refs.formEl.addEventListener('submit', formHandler);
 refs.brtMoreEl.addEventListener('click', moreImageOnClick);
 
-function moreImageOnClick(evt) {
-  refs.brtMoreEl.setAttribute('hidden', 'true');
+async function moreImageOnClick() {
   countOfPage += 1;
-  searchImage(qValue, countOfPage)
-    .then(data => {
-      data.hits.forEach(item => {
-        renderMarkup(
-          item.webformatURL,
-          item.largeImageURL,
-          item.tags,
-          item.views,
-          item.likes,
-          item.comments,
-          item.downloads
-        );
-      });
-    })
-    .catch(error => console.log(error.message));
+  try {
+    const data = await searchImage(qValue, countOfPage);
+    refs.brtMoreEl.classList.toggle('hidden');
+    totalHits -= NUMBER_OF_IMAGE;
+    if (totalHits <= NUMBER_OF_IMAGE) {
+      refs.brtMoreEl.classList.add('hidden');
+      refs.endOfListEL.classList.remove('hidden');
+    }
+    data.hits.forEach(item => {
+      renderMarkup(
+        item.webformatURL,
+        item.largeImageURL,
+        item.tags,
+        item.views,
+        item.likes,
+        item.comments,
+        item.downloads
+      );
+    });
+    refs.brtMoreEl.classList.toggle('hidden');
+  } catch (error) {
+    Notiflix.Notify.failure(`Sorry, ${error.message}`);
+  }
 }
 
-function formHandler(evt) {
+async function formHandler(evt) {
   evt.preventDefault();
   countOfPage = 1;
-  refs.brtMoreEl.setAttribute('hidden', 'true');
-  qValue = evt.target.elements[0].value;
-  searchImage(qValue, countOfPage)
-    .then(data => {
+  qValue = evt.target.elements[0].value.trim();
+  refs.brtMoreEl.classList.remove('hidden');
+  refs.endOfListEL.classList.add('hidden');
+  if (qValue === '') {
+    Notiflix.Notify.failure('Sorry, the search must not be empty');
+  } else {
+    try {
+      const data = await searchImage(qValue, countOfPage);
+      totalHits = data.totalHits;
+      console.log(totalHits);
+      if (totalHits <= NUMBER_OF_IMAGE) {
+        refs.brtMoreEl.classList.add('hidden');
+        refs.endOfListEL.classList.remove('hidden');
+      }
       if (!data.hits.length) {
         refs.galleryEl.innerHTML = '';
         Notiflix.Notify.failure(
@@ -65,14 +85,16 @@ function formHandler(evt) {
           );
         });
       }
-    })
-    .catch(error => console.log(error.message));
+    } catch (error) {
+      Notiflix.Notify.failure(`Sorry, ${error.message}`);
+    }
+  }
 }
 
 async function searchImage(qValue, countOfPage) {
   const response = await axios.get(`?key=${API_KEY}&q=
   ${qValue}&image_type=${IMAGE_TYPE}&orientation=${IMAGE_ORIENTATION}&
-  safesearch=${IMAGE_SAFESEARCH}&per_page=40&page=${countOfPage}`);
+  safesearch=${IMAGE_SAFESEARCH}&per_page=${NUMBER_OF_IMAGE}&page=${countOfPage}`);
   const imagesInfo = await response.data;
   return imagesInfo;
 }
@@ -86,10 +108,11 @@ function renderMarkup(
   comments,
   downloads
 ) {
+  refs.brtMoreEl.classList.toggle('hidden');
   refs.galleryEl.insertAdjacentHTML(
     'beforeend',
     `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="100%" height="300"/>
   <div class="info">
     <p class="info-item">
       <b>Likes: ${likes}</b>
@@ -106,5 +129,4 @@ function renderMarkup(
   </div>
 </div>`
   );
-  refs.brtMoreEl.removeAttribute('hidden');
 }
